e92e71716af3f37f1e0018e90e1531997ec8e537
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-10 16:07:09 +0530

    day-1

---
+++ 1_print_basic_scripts.py
@@ -0,0 +1,32 @@
+# Task 1: Print "Hello, World!"
+print("Hello, World")
+
+# Task 2: Print multiple lines
+print("I'm learning python")
+print("This is so much fun")
+print("I can't wait to learn more")
+
+# Task 3: Use single and double quotes
+print('this is a string with single quotes')
+print("this is a string with double quotes")
+
+# Task 4: Print strings with quotes inside
+print("And then he said \'I love you\'")
+
+# Task 5: Use escape characters
+print("hello how are you doing, \n this is second line,\t  this is tabbed, ")
+
+
+# Task 6 Combine different data types
+x = "python"
+y = 30
+print("My name is " + x + " and i am a 30 years old")
+
+
+# Task 7: Get creative!
+print("So the story took place in 2085,\n there was a guy an Ex security researcher \n Who fell in love with someone else. \n blablabla")
+
+# Hint: Remember to use print() for each task
+# Hint: For quotes inside strings, try using the opposite quote type
+# Hint: \n creates a new line, \t creates a tab
+# Hint: Separate different data types with commas in print()
---
+++ 2_multiline_printing.py
@@ -0,0 +1,27 @@
+#different ways to go with multiline strings
+
+print("line 1 \n line 2 \n line 3")
+
+#multiple line print
+print("line 1 ")
+print("line 2 ")
+print("line 2 ")
+
+#multiline print
+print("""this is line 1.
+this is line 2.
+this is line 3.
+""")
+
+#you can also use single quotes instead
+
+print('''this is line 1.
+this is line 2.
+this is line 3.
+''')
+
+#escape charactors
+# \t - adds a tab
+# \ - print a backslash
+# \" print a double quote"
+# \' prints a single quote
