e52839b7e8cd4bd480a14427f28c700fce14ecd2
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-11 19:57:32 +0530

    day-2

---
+++ datatypes_in_python.py
@@ -0,0 +1,23 @@
+#common datatypes
+#Numbers
+1 ,2, 3#Integers
+3.14, -0.12 #floats (float)
+#Text
+'this is a string' #
+True, false # this is a boolean
+#
+#
+age = 27  #age is just a integer (int)
+pi = 3.14 #pi is float
+name = 'Helen' #name is a string (str)
+
+is_new = True #is_new a boolean (bool)
+#
+#
+#
+#
+#
+#
+#
+#
+#
