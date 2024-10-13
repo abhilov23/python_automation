89549e5561c428e376f07e15c58ded5f62bc5874
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-13 13:39:35 +0530

    Day-4
    basic calc regarding python

---
+++ 5_saving calculator.py
@@ -0,0 +1,25 @@
+#Saving calculator starter file
+
+#Setting up variable
+#Create variables for monthly saving here
+month1 = 45
+month2 = 54
+month3 = 67
+
+#task 2: calculating total savings
+# calculate total savings here
+
+total_savings = month1 + month2 + month3#your code here
+
+#task 3 : calculating average savings
+#calculate average savings here
+
+num_months = 3#your code here
+average_savings = total_savings/num_months 
+
+#task 4 : formatting and printing results
+#print the formatted results here
+#your code here
+
+#using variables in print statements
+print(f"total savings over {num_months} is {average_savings}")
