821b175b69a3148d96ee0debcc2ea7570a90e721
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-15 23:36:47 +0530

    day-5

---
+++ 6_smart_bill_splitter.py
@@ -0,0 +1,8 @@
+#lets create  a bill spitter script
+# total bill:$128
+#Number of friends: 5
+#goal: Calculate how much each person should pay
+
+#Bonus :
+# 1. Include a 15% tip in the total bill before splitting
+# 2. Round the individual share to two decimal places
