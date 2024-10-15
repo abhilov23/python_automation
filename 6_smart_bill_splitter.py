17f7b1ef727c52bc4726e593e4354b3db6cd3bee
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-15 23:46:37 +0530

    day-6
    here is the calculator program

--- 6_smart_bill_splitter.py
+++ 6_smart_bill_splitter.py
@@ -5,4 +5,14 @@
 
 #Bonus :
 # 1. Include a 15% tip in the total bill before splitting
-# 2. Round the individual share to two decimal places
+# 2. Round the individual share to two decimal places (hint: look up to the round() function)
+
+total_bill = 128 
+no_of_friends = 5 
+
+#calculate tip
+tip = (15 * total_bill) / 100
+total_bill = total_bill + tip   #adding the tip in the total bill
+per_person = round(total_bill/no_of_friends, 2) #round
+
+print("total bill per person:",per_person )
