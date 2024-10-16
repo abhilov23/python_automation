fdda8723bda2d7b1a62a7d96e65768fcefbbdbc4
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-16 22:21:29 +0530

    day-6

---
+++ 8_operation_performing_order.py
@@ -0,0 +1,10 @@
+#- Python follows PEMDAS:
+#1. Parentheses ()
+#2. Exponential **
+#3. Multiplication *, Division / , Integer Division //, Modulus %
+#4. Addition +, Subtraction -
+
+print((5 + 3) * 2) #paramtheses
+
+print(2 ** 3 * 2) #exponential
+print(20 / 4 * 2)
---
+++ 9_PEP-8.py
@@ -0,0 +1,9 @@
+#using naming convections
+def calculate_area(length, width): #good
+def CalculateArea(): #not recommended
+
+my_variable = 45 #good
+myVariable = 45 #not recommended
+
+class CarEngine: #good
+class car_engine: #not recommended
