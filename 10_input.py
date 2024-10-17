5bdb3415f2ca8ba94bd98cb7adcf097a1b1ccaf2
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-17 12:20:20 +0530

    day-7

---
+++ 10_input.py
@@ -0,0 +1,10 @@
+#to take input from the user, we use a 
+#method i.e: input()
+a = input()
+print(a)
+#but eventually we need to know which type of input you need to go with
+#and for that you need to use type function
+
+a = input("Enter a number: ") #you can also add the comments in your input function
+print(type(a)) #and this will reflect the type of input you are taking
+
---
+++ 11_bday.py
@@ -0,0 +1,7 @@
+#taking input from the users
+
+current_year = 2024
+birthday_year = input("enter birthday year ")
+
+age = current_year - birthday_year
+print("your approximate age is " + age)
