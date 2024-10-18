b6b3e0295e8492e544b3568b9def7bb3c932d9c3
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-18 23:44:23 +0530

    day-7

---
+++ 13_lists.py
@@ -0,0 +1,15 @@
+#lists in python
+#creating lists
+fruits = ['banana', 'apple', 'orange', 'cherry']
+number = [1, 2,3,4,5,6,7,8,9,10,11,12]
+mixed = ["banana", 45, [54, 5]]
+
+#lets talk about slicing list: [start:end:steps]
+print(number[2:6:1]) #3,4,5,6
+print(number[:4]) #from starting to 4th position
+print(number[4:]) #from 4 to the last elements
+print(number[::2])#from the first elements to the last after every two steps
+
+
+repeated = 2 * fruits 
+print(repeated) #it prints the list for two times
